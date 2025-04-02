456789753d3d0c155ce61b0c1b9f084451454608
gasparnd <gaspardolcemascolo@gmail.com>
2025-04-02 19:18:02 -0300

    Initial Commit

---
+++ HackerNewsReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackerNewsReader
+//
+//  Created by Gaspar Dolcemascolo on 02-04-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HackerNewsReaderUITests/HackerNewsReaderUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  HackerNewsReaderUITestsLaunchTests.swift
+//  HackerNewsReaderUITests
+//
+//  Created by Gaspar Dolcemascolo on 02-04-25.
+//
+
+import XCTest
+
+final class HackerNewsReaderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
